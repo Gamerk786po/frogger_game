e23cf66d4c67a1b2a81287c71ab8cc68667a2e9d
Gamerk786po <abdullahpper@gmail.com>
2026-07-31 22:46:56 +0500

    first commit

---
+++ scenes/player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var direction: Vector2 = Vector2(0,0);
+var speed: int = 150
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	animation()
+	velocity = direction * speed
+	move_and_slide()
+
+func animation() -> void:
+	if direction:
+		$AnimatedSprite2D.flip_h = (direction.x > 0)
+	else:
+		$AnimatedSprite2D.frame = 0
+	
