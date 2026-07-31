e20666a2c8a8b9c0eab8224525b28c932685a1d4
Gamerk786po <abdullahpper@gmail.com>
2026-08-01 01:20:38 +0500

    scope(player) up and down animations added

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var direction: Vector2 = Vector2(0,0);
+var direction: Vector2 = Vector2(0, 0);
 var speed: int = 150
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,8 +15,15 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 
 func animation() -> void:
+	# When direction is true => means user is constantly trying to move
 	if direction:
+		# Fling depends on the direction of x axis, when x is negative
+		# user move left and when x is positive user moves right
 		$AnimatedSprite2D.flip_h = (direction.x > 0)
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = "horizontal"
+		else:
+			if direction.y != 0:
+				$AnimatedSprite2D.animation = "down" if direction.y > 0 else "up"
 	else:
 		$AnimatedSprite2D.frame = 0
-	
