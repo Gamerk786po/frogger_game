bbe39fe1417512ce8c72a318a6706d374714103b
Gamerk786po <abdullahpper@gmail.com>
2026-08-13 02:04:56 +0500

    feat(Game) Add Score to Title and Add a Event to load Game When SpaceBar is pressed

---
+++ scenes/Title/title.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label2.text = "Score: " + str(Global.score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if(Input.is_action_just_pressed("End")):
+		call_deferred("go_to_game")
+
+func go_to_game() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
--- scenes/game.gd
+++ scenes/game.gd
@@ -3,7 +3,7 @@ extends Node2D
 # Variable for Score
 var score:int 
 # Preloading car_scene
-var car_scene: PackedScene = preload("res://scenes/car.tscn")
+var car_scene: PackedScene = preload("res://scenes/car/car.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +14,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-
+# Car Spawning
 func _on_car_timer_timeout() -> void:
 	# Getting car instantiated
 	var car = car_scene.instantiate() as Area2D
@@ -24,11 +24,15 @@ func _on_car_timer_timeout() -> void:
 	$cars.add_child(car)
 	car.connect("body_entered", player_collided)
 
-func player_collided(body) -> void :
-	print(body)
-	print("Game Over!")
+# Go to title when player collide with the car
+func player_collided(_body) -> void :
+	call_deferred("go_to_title")
+	Global.score = score
 
+func go_to_title() -> void:
+	get_tree().change_scene_to_file("res://scenes/Title/title.tscn")
 
+# Updating score
 func _on_score_timer_timeout() -> void:
 	score += 1
 	$CanvasLayer/Label.text = str(score)
